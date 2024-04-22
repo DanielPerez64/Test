6d6e85167f0fa8c2a89aae52f2453db1c040203c
DanielPerez64 <pdpm4505@gmail.com>
2024-04-22 09:53:08 -0600

    Fist commit

---
+++ Debug/source/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/semihost_hardfault.c \
+../source/test_setup.c 
+
+C_DEPS += \
+./source/semihost_hardfault.d \
+./source/test_setup.d 
+
+OBJS += \
+./source/semihost_hardfault.o \
+./source/test_setup.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/%.o: ../source/%.c source/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DCPU_MK64FN1M0VLL12 -DCPU_MK64FN1M0VLL12_cm4 -DSDK_OS_BAREMETAL -DSDK_DEBUGCONSOLE=1 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DSERIAL_PORT_TYPE_UART=1 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"C:\TAE_DM\SDK\test_setup\board" -I"C:\TAE_DM\SDK\test_setup\source" -I"C:\TAE_DM\SDK\test_setup\utilities" -I"C:\TAE_DM\SDK\test_setup\drivers" -I"C:\TAE_DM\SDK\test_setup\device" -I"C:\TAE_DM\SDK\test_setup\component\serial_manager" -I"C:\TAE_DM\SDK\test_setup\component\lists" -I"C:\TAE_DM\SDK\test_setup\CMSIS" -I"C:\TAE_DM\SDK\test_setup\component\uart" -O0 -fno-common -g3 -gdwarf-4 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-source
+
+clean-source:
+	-$(RM) ./source/semihost_hardfault.d ./source/semihost_hardfault.o ./source/test_setup.d ./source/test_setup.o
+
+.PHONY: clean-source
+
